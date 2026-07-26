265a7cea60027b4d3451439399beb2311282a6de
Augustin Dinulica <dinulicaaugustin@gmail.com>
2026-07-26 11:01:10 +0300

    chore: adds prettier

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
-const baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000/api';
+const baseURL =
+  import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000/api';
 
-if(!baseURL) {
-  throw new Error('API base URL is not defined. Please set the VITE_API_BASE_URL environment variable.');
+if (!baseURL) {
+  throw new Error(
+    'API base URL is not defined. Please set the VITE_API_BASE_URL environment variable.',
+  );
 }
 
 const apiClient = axios.create({
